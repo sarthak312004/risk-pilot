b2608087404aeac7ea3544b050a1c847922cbb61
srthk31 <sarthak31122004m@gmail.com>
2026-06-29 21:17:07 +0530

    Fix:Change Event and update the calculation logic

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-
-document.querySelector('.inputs input').addEventListener('keydown', calculate())
-
 function calculate(){
 
-    return function(){
-        
     const inputData = document.querySelectorAll('.inputs input')
     let count = 0
     inputData.forEach((data)=>{
-        if(!data.value){
-            console.log("fill required inputs !");
-        }else{
-           count++
+        if(data.value){
+              count++
         }
     })
     if(count === 4){
@@ -20,20 +13,30 @@ function calculate(){
         let entry = Number(inputData[2].value)
         let stoploss = Number(inputData[3].value)
 
-        let totalRisk = (capital / 100) * risk
-        let diff = entry - stoploss
-        let capitalNeed = (totalRisk / diff) * entry
-        document.querySelector('#risk').innerHTML = `Risk: ${risk}% ~${totalRisk}₹`
+        document.querySelector('#risk').innerHTML = `Risk: ${risk}% ~${(capital / 100) * risk}₹`
 
-        document.querySelector('#lot-size').innerHTML = `Lot Size: ${totalRisk / diff}`
-
-        if(capitalNeed > capital){
-            document.querySelector('#capital').style.color = "orange"
-            document.querySelector('#capital').innerHTML = `Capital: ${capitalNeed}₹`
+        if(entry < stoploss){
+            document.querySelector('#lot-size').innerHTML = `Lot Size: ${Math.trunc(((capital / 100) * risk) / (stoploss - entry))}`
         }else{
-            document.querySelector('#capital').innerHTML = `Capital: ${capitalNeed}₹`
+            document.querySelector('#lot-size').innerHTML = `Lot Size: ${Math.trunc(((capital / 100) * risk) / (entry - stoploss))}`
         }
-        // console.log(capital, risk, entry, stoploss);
-    }
+
+        if(((((capital / 100) * risk) / (entry - stoploss)) * entry) > capital){
+            document.querySelector('#capital').style.color = "orange"
+            document.querySelector('#capital').innerHTML = `Capital: ${(((capital / 100) * risk) / (entry - stoploss)) * entry}₹`
+        }else{
+            document.querySelector('#capital').style.color = "black"
+            if(entry < stoploss){
+                document.querySelector('#capital').innerHTML = `Capital: ${Math.trunc((((capital / 100) * risk) / (stoploss - entry)) * entry)}₹`
+            }else{
+                document.querySelector('#capital').innerHTML = `Capital: ${Math.trunc((((capital / 100) * risk) / (entry - stoploss)) * entry)}₹`
+            }
+        }
     }
 }
+
+document.getElementById('account-size-input').addEventListener('input', calculate)
+document.getElementById('risk-input').addEventListener('input', calculate)
+document.getElementById('entry-input').addEventListener('input', calculate)
+document.getElementById('sl-input').addEventListener('input', calculate)
+
