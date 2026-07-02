fbe00a5663c3f3d40b161aa2a14c23cae92bf111
srthk31 <sarthak31122004m@gmail.com>
2026-07-02 13:17:37 +0530

    Feat: add pie chart, fixed refresh logic and make code clean

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let myPieChart = null;
 function calculate() {
   const inputData = document.querySelectorAll(".inputs input");
   let count = 0;
@@ -7,36 +8,48 @@ function calculate() {
     }
   });
   if (count === 4) {
-    let capital = Number(inputData[0].value);
-    let risk = Number(inputData[1].value);
-    let entry = Number(inputData[2].value);
-    let stoploss = Number(inputData[3].value);
+    const capital = Number(inputData[0].value);
+    const risk = Number(inputData[1].value);
+    const entry = Number(inputData[2].value);
+    const stoploss = Number(inputData[3].value);
+
+    const riskAmount = (capital / 100) * risk;
+    const priceDiff = entry < stoploss ? stoploss - entry : entry - stoploss;
+    const lotSize = Math.trunc(riskAmount / priceDiff);
+    const capitalRequired = lotSize * entry;
 
     document.querySelector("#risk").innerHTML =
-      `Risk: ${risk}% ~${(capital / 100) * risk}₹`;
+      `Risk: ${risk}% ~${riskAmount}₹`;
+    document.querySelector("#lot-size").innerHTML = `Lot Size: ${lotSize}`;
 
-    if (entry < stoploss) {
-      document.querySelector("#lot-size").innerHTML =
-        `Lot Size: ${Math.trunc(((capital / 100) * risk) / (stoploss - entry))}`;
+    const capitalElement = document.querySelector("#capital");
+    if (capitalRequired > capital) {
+      capitalElement.style.color = "orange";
+      capitalElement.innerHTML = `Capital: ${capitalRequired}₹`;
     } else {
-      document.querySelector("#lot-size").innerHTML =
-        `Lot Size: ${Math.trunc(((capital / 100) * risk) / (entry - stoploss))}`;
+      capitalElement.style.color = "black";
+      capitalElement.innerHTML = `Capital: ${Math.trunc(capitalRequired)}₹`;
     }
 
-    if ((((capital / 100) * risk) / (entry - stoploss)) * entry > capital) {
-      document.querySelector("#capital").style.color = "orange";
-      document.querySelector("#capital").innerHTML =
-        `Capital: ${(((capital / 100) * risk) / (entry - stoploss)) * entry}₹`;
-    } else {
-      document.querySelector("#capital").style.color = "black";
-      if (entry < stoploss) {
-        document.querySelector("#capital").innerHTML =
-          `Capital: ${Math.trunc((((capital / 100) * risk) / (stoploss - entry)) * entry)}₹`;
-      } else {
-        document.querySelector("#capital").innerHTML =
-          `Capital: ${Math.trunc((((capital / 100) * risk) / (entry - stoploss)) * entry)}₹`;
-      }
+    if (myPieChart) {
+      myPieChart.destroy();
     }
+
+    const ctx = document.querySelector(".pie-chart");
+    myPieChart = new Chart(ctx, {
+      // Assign to myPieChart variable
+      type: "pie",
+      data: {
+        labels: ["Risk", "Capital", "Postion size"],
+        datasets: [
+          {
+            data: [riskAmount, capitalRequired, lotSize],
+            backgroundColor: ["#ff6384", "#36eb4e", "#56f9ff"],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
   }
 }
 
@@ -49,27 +62,34 @@ document.getElementById("sl-input").addEventListener("input", calculate);
 
 // createCards
 let isCreated = false;
+let coinDiv;
+let coinName;
+let coinPrice;
 function createCards(data) {
-  if (data && !isCreated) {
+  if (isCreated) {
+    const allDiv = document.querySelectorAll(".coin-card-container div");
+    allDiv.forEach((card) => {
+      card.remove();
+    });
+    isCreated = false;
+  }
+  if (!isCreated) {
     for (let [key, value] of Object.entries(data)) {
-      const coinDiv = document.createElement("div");
+      coinDiv = document.createElement("div");
 
-      let coinName = document.createElement("h4");
+      coinName = document.createElement("h4");
       coinName.id = "coin-name";
       coinName.innerText = key.toUpperCase();
       coinDiv.appendChild(coinName);
 
-      let coinPrice = document.createElement("p");
+      coinPrice = document.createElement("p");
       coinPrice.id = "coin-price";
-      coinPrice.innerText = value.usd;
+      coinPrice.innerText = `$ ${value.usd}`;
       coinDiv.appendChild(coinPrice);
 
       document.querySelector(".coin-card-container").appendChild(coinDiv);
       isCreated = true;
     }
-  } else if (isCreated) {
-    coinName.innerText = key.toUpperCase();
-    coinPrice.innerText = value.usd;
   }
 }
 
@@ -86,8 +106,9 @@ async function fetchPrices() {
     console.log(error);
   }
 }
+
 setInterval(()=>{
     fetchPrices()
-},20000)
+},10000)
 
 document.addEventListener("DOMContentLoaded", fetchPrices);
